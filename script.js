452dff64bfbff7c7e41b3f023cd1c175c5d84a60
Tun Lin <lintun2003@gmail.com>
2024-05-22 18:28:41 -0400

    add scoreboard and winner announcement

--- script.js
+++ script.js
@@ -29,22 +29,37 @@ function playGame() {
   let win = 0;
   let lose = 0;
   let tie = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt('enter rock, paper scissor')
-    const computerSelection = getComputerChoice().toLowerCase();
-    let buffer = playRound(playerSelection, computerSelection);
-    console.log(buffer);
-    if (buffer === "you win") {
-      win++;
-    } else if (buffer === "you lose") {
-      lose++;
-    } else {
-      tie++;
-    }
-  }
-  console.log("win", win);
-  console.log("lose", lose);
-  console.log("tie", tie);
+
+  const playerSelection = document.querySelectorAll("button");
+  playerSelection.forEach((playerSelection) => {
+    playerSelection.addEventListener("click", () => {
+      const computerSelection = getComputerChoice().toLowerCase();
+      let result = playRound(playerSelection.id, computerSelection);
+      console.log(result);
+      if (result === "you win") {
+        win++;
+      } else if (result === "you lose") {
+        lose++;
+      } else {
+        tie++;
+      }
+      const score = document.querySelector("div");
+      score.textContent =
+        "Wins: " + win + " " + "Lose: " + lose + " " + "Ties: " + tie;
+
+      function resetScore(message) {
+        score.textContent = message;
+        win = 0;
+        lose = 0;
+        tie = 0;
+      }
+      if (win === 5) {
+        resetScore("You Win!");
+      } else if (lose === 5) {
+        resetScore("You Lose!");
+      }
+    });
+  });
 }
 
 playGame();
